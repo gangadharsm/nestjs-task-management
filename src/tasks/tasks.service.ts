897bf8f8ff99da69c47f10782b3dbdef9fefe94c
GANGADHAR <prasannav@commitservices.net>
2022-07-23 22:54:43 +0530

    tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { NotFoundError } from 'rxjs';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -42,11 +43,16 @@ export class TasksService {
   };
 
   getTaskById = (id: string): Task => {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with id "${id}" not found`);
+    }
+    return found;
   };
 
   deleteTask = (id: string): void => {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   };
 
   updateTaskStatus = (id: string, status: TaskStatus): Task => {
