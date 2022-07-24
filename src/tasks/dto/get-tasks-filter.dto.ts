c8188a8da4a7ae5bb7ae8f7dd7b687a195a4b152
GANGADHAR <prasannav@commitservices.net>
2022-07-24 13:40:20 +0530

    changes

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 /* eslint-disable prettier/prettier */
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
