a16bbba1934c750cc3744f62302091f01ff35339
GANGADHAR <prasannav@commitservices.net>
2022-07-27 11:50:56 +0530

    changes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,8 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { NotFoundError } from 'rxjs';
-import { TaskRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
